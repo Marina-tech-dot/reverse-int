3e460f29a5b480dbbe67535001f9bfc81a3f455c
Marina-tech-dot <marina.juriskonsult.gmail.com>
2022-01-04 11:45:12 +0100

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    
+    (typeof n === 'number') ? n = n.toFixed() : 0;
+
+    let numReverse = n.split("").reverse().join("");
+
+    let numReverseCut = numReverse.slice(0, -1);
+
+    return (numReverse[numReverse.length - 1] === "-") ? numReverseCut : numReverse;
 }
